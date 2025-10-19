6fda36113b3cbd536fd90f551f67362fc1ff4aed
itsmecia <118735762+itsmecia@users.noreply.github.com>
2025-10-19 15:42:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -761,7 +761,7 @@ elif mode == "Rute Pengangkutan":
                     
                     # insight 
                     urutan_tps = " ➜ ".join([str(r.get("id_tps")) for r in route])
-                    st.markdown("### Insight)
+                    st.markdown("### Insight")
                     st.write(f"- **Rute terpendek yang direkomendasikan:** {urutan_tps} ➜ {nearest_tpa.get('nama','-')}")
                     st.write(f"- **Total jarak tempuh:** {total_distance:.2f} km untuk {len(selected_tps)} TPS.")
                     st.write(f"- **Rata-rata jarak antar segmen:** {total_distance/len(selected_tps):.2f} km.")
@@ -1115,6 +1115,7 @@ elif mode == "Prediksi Volume Sampah":
             
             
     
+
 
 
 
