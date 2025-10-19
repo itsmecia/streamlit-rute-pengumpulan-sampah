755a8769bc88a00414c2b490f030788cb466c63b
itsmecia <118735762+itsmecia@users.noreply.github.com>
2025-10-19 12:58:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -273,25 +273,30 @@ if mode == "Dashboard Data":
     
     ## Tambahkan legenda di dalam peta
     legend_html = """
-    <div style="
-         position: absolute; 
-         bottom: 20px; right: 20px;
-         z-index: 9999;
-         background-color: rgba(255, 255, 255, 0.9);
-         border: 1px solid #888;
-         border-radius: 8px;
-         padding: 8px 12px;
-         font-size: 13px;
-         line-height: 1.4;
-         box-shadow: 1px 1px 4px rgba(0,0,0,0.3);
-         max-width: 130px;
-         overflow-wrap: break-word;
-    ">
+    <style>
+        .maplegend {
+            position: absolute;
+            bottom: 25px;
+            right: 25px;
+            z-index:9999;
+            background-color: rgba(255, 255, 255, 0.9);
+            border: 1px solid #888;
+            border-radius: 8px;
+            padding: 8px 12px;
+            font-size: 13px;
+            line-height: 1.4;
+            box-shadow: 1px 1px 4px rgba(0,0,0,0.3);
+            max-width: 130px;
+        }
+    </style>
+    
+    <div class='maplegend'>
     <b>Legenda:</b><br>
     <i class="fa fa-trash" style="color:green; margin-right:5px;"></i> TPS<br>
     <i class="fa fa-recycle" style="color:red; margin-right:5px;"></i> TPA
     </div>
     """
+    
     m.get_root().html.add_child(folium.Element(legend_html))
 
 
@@ -1104,6 +1109,7 @@ elif mode == "Prediksi Volume Sampah":
             
             
     
+
 
 
 
