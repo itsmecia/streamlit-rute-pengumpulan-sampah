20e59a2705ddcc726b9cb6889f5d5e026063317b
itsmecia <118735762+itsmecia@users.noreply.github.com>
2025-10-21 15:18:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.markdown("""
 <style>
 /* WARNA DASAR*/
 html, body, .main, .block-container {
-    background-color: #E1F5E1 !important;
+    background-color: #DFF1F1 !important;
     font-family: 'Poppins', sans-serif;
     color: #000000 !important;
 }
@@ -1418,6 +1418,7 @@ elif mode == "Prediksi Volume Sampah":
             
             
     
+
 
 
 
