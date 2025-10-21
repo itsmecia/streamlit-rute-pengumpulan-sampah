6d9616d6c1243938257e7342462ba1d47f4643cc
itsmecia <118735762+itsmecia@users.noreply.github.com>
2025-10-21 11:51:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -269,13 +269,14 @@ st.sidebar.markdown("<h3 style='text-align:center;'>📂 Info Dataset</h3>", uns
 st.sidebar.markdown(f'''
 <div style="background-color:#fff; border-radius:10px; padding:12px; margin-top:12px;
 font-size:14px; color:#1b4d3e; box-shadow:0 2px 6px rgba(0,0,0,0.1);">
-<b>tps.csv</b> – 76 baris<br>
-<b>tpa.csv</b> – 10 baris<br>
-<b>histori_rute.csv</b> – 50 baris<br>
-<b>routes.csv</b> – 32 baris<br>
-<b>vehicle_routing_matrix.csv</b> – 20 baris
+<b>tps.csv</b> – {len(tps_df)} baris<br>
+<b>tpa.csv</b> – {len(tpa_df)} baris<br>
+<b>histori_rute.csv</b> – {len(histori_df)} baris<br>
+<b>routes.csv</b> – {len(routes_df)} baris<br>
+<b>vehicle_routing_matrix.csv</b> – {len(vehicle_df)} baris
 </div>
 ''', unsafe_allow_html=True)
+
 
 st.sidebar.markdown("""
 <div style='text-align:center; font-size:12px; margin-top:15px; opacity:0.7'>
@@ -1405,6 +1406,7 @@ elif mode == "Prediksi Volume Sampah":
             
             
     
+
 
 
 
