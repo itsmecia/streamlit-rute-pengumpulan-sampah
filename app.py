981ae1c0d496e92617917a20bafa1fe7622a7416
itsmecia <118735762+itsmecia@users.noreply.github.com>
2025-10-21 20:53:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ h1, h2, h3 {
 
 /* METRIK */ 
 [data-testid="stMetricValue"] { 
-    color: #649A66 !important; 
+    color: #7DAA7E !important; 
 } 
 
 /* INFO CARD */ 
@@ -1476,6 +1476,7 @@ elif mode == "Prediksi Volume Sampah":
             
             
     
+
 
 
 
