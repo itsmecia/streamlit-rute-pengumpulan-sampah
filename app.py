d09cd94152fd848b08c5cfa648ee82079338fe69
itsmecia <118735762+itsmecia@users.noreply.github.com>
2025-10-21 14:03:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,10 @@ h1, h2, h3 {
 
 #header
 st.markdown("<h2 style='font-weight:bold; text-align:center;'>🌍 Sistem Analisis Rute & Pengumpulan Sampah</h2>", unsafe_allow_html=True)
-st.markdown("<h4 style='text-align:center;'>Analitik dan optimasi rute pengangkutan sampah berbasis Big Data.</h4>", unsafe_allow_html=True)
+st.markdown(
+    "<p style='text-align:center; font-weight:normal; font-size:18px; margin-top:0;'>Analitik dan optimasi rute pengangkutan sampah berbasis Big Data</p>",
+    unsafe_allow_html=True
+)
 st.markdown("---")
 
 # data
@@ -1435,6 +1438,7 @@ elif mode == "Prediksi Volume Sampah":
             
             
     
+
 
 
 
